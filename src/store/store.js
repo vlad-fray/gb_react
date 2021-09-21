674079708f6ce007a98b6d33887b889f8a302692
VFray <rekrut313@gmail.com>
2021-09-21 18:20:43 +0300

    redux devtools

--- src/store/store.js
+++ src/store/store.js
@@ -6,4 +6,7 @@ const reducers = combineReducers({
   dialogs: dialogsReducer,
   profile: profileReducer,
 });
-export const store = createStore(reducers);
+export const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
